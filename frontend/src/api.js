21a223fa5f7757e55b76a4f660f55f03887f6cc8
Rkmishra123 <rajkumar9798314825@gmail.com>
2026-04-16 22:50:14 +0530

    connect deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:7000/api'
+  baseURL: 'https://your-url.up.railway.app/api'
 });
 
 api.interceptors.request.use(cfg => {
