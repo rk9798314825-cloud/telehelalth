902c88b14245e74ca8e2d2627f9a8279c4283263
Rkmishra123 <rajkumar9798314825@gmail.com>
2026-04-17 00:03:21 +0530

    connect correct backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://your-url.up.railway.app/api'
+  baseURL: 'https://telemedicine-project-production-8bf7.up.railway.app/api'
 });
 
 api.interceptors.request.use(cfg => {
